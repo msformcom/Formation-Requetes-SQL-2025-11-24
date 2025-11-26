f91d437e3d1bb7026ae71df3b47096cdba19b16a
Dominique MAURAS <contact@msform.com>
2025-11-26 14:58:11 +0100

    update auto

--- Code/Select/Fonction Fenetres/Fonctions fenetres.sql
+++ Code/Select/Fonction Fenetres/Fonctions fenetres.sql
@@ -12,7 +12,8 @@ SELECT * ,
 	--SUM(unitprice) OVER (PARTITION BY Categoryid  ) ASSommePrixParCategorie
 		LAG(unitprice) OVER (PARTITION BY Categoryid ORDER BY unitprice ASC ) AS PrixduPrecedent,
 		LEAD(unitprice) OVER (PARTITION BY Categoryid ORDER BY unitprice ASC ) AS PrixduSuivant,
-		AVG(unitprice) OVER (PARTITION BY Categoryid ORDER BY unitprice ASC ROWS BETWEEN 3 PRECEDING AND 1 PRECEDING ) AS TT
+		AVG(unitprice) OVER (PARTITION BY Categoryid ORDER BY unitprice ASC ROWS BETWEEN 3 PRECEDING AND 1 PRECEDING ) AS TT,
+		SUM(unitprice) OVER (PARTITION BY Categoryid ORDER BY unitprice ASC ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING ) AS T2
 FROM Production.Products) AS T
 -- WHERE RowNumberPrix<=5
 ORDER BY categoryid, unitprice
